da1557c411c9d16c3f860bfdd222866a347e0b90
Zayox <amirmoumen06@yahoo.fr>
2022-03-04 01:55:44 +0100

    banner updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
     return (
         <div className="ml-[5vw] flex">
             <div className="mt-40">
-                <h1 className="font-bold text-6xl bg-sky-400 w-[26rem] lg:w-[41rem] p-2 lg:text-8xl">SocialShadow,</h1>
+                <h1 className="font-bold text-6xl bg-sky-400 w-[26rem] lg:w-[41rem] p-2 lg:text-8xl text-white">SocialShadow,</h1>
                 <h1 className="font-bold text-6xl mt-4 lg:text-8xl">Une plateforme sûre</h1>
                 <h1 className="font-bold text-6xl mt-4 lg:text-8xl">Pour un monde obscur</h1>
             </div>
