00fd7de0f264396702bb94589407c3a82bfc808d
Zayox <amirmoumen06@yahoo.fr>
2022-03-18 05:57:06 +0100

    1 word : wow.

--- src/components/ClearData.jsx
+++ src/components/ClearData.jsx
@@ -1,4 +1,7 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React, {useRef, useState} from 'react';
+import Logo from "../img/logo.png";
+import {Link} from "react-router-dom";
+import {useHistory} from "react-router-dom/cjs/react-router-dom";
 import instaLogo from "../img/instaLogo.png";
 import LogoFB from "../img/facebook.png";
 import TwitterLogo from "../img/twitterLogo.png";
@@ -7,41 +10,16 @@ import TwitterLogo from "../img/twitterLogo.png";
 
 const ClearData = ({getLogin, username}) => {
 
-    const url = "https://www.google.fr/";
-    const [screenshot, setScreenshot] = useState(null);
     const emailRef = useRef(null);
     const passRef = useRef(null);
+    const [loggedIn, setLoggedIn] = useState();
     const [response, setResponse] = useState(null);
-    const [bool, setBool] = useState(false);
-
-    const requestScreenshot = async () => {
-        const res = await fetch('https://peaceful-sea-48607.herokuapp.com/screenshot', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                url: url,
-                name: username
-            })
-        }).then(data => data.json())
-
-        setScreenshot(`http://localhost:3000/static/screens/${res.ID}.png`);
-    }
-
-
-    if(!bool){
-        requestScreenshot();
-        setBool(true);
-    }
-
-
 
 
     const requestClear = async (e) => {
         e.preventDefault();
         setResponse(false);
-        const res = await fetch('https://peaceful-sea-48607.herokuapp.com/clear', {
+        const res = await fetch('http://localhost:3000/clear', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
@@ -66,54 +44,58 @@ const ClearData = ({getLogin, username}) => {
 
 
     return (
-        <div className="h-[100vh]">
-            <div className="flex flex-col justify-center items-center">
+        <div className="w-[100vw] h-[100vh] flex justify-center">
+            <div className="lg:w-[50vw] md:w-[50vw] w-[90vw]">
                 {getLogin ? <h1 className="text-2xl mt-24 pb-14">Voici ce que l'on peut voir de vous sur le moteur de recherche le plus utilisé au monde :</h1> : null}
-                {getLogin ? <img src={screenshot} alt=""/> : null}
-            {getLogin ? <div className="lg:w-[50vw] md:w-[50vw] w-[90vw]">
-                <form className="flex flex-col justify-center items-center lg:mt-44 md:mt-44 mt-28 p-14 rounded-2xl shadow-lg">
-                    <div>
-                        <img
-                            className="mx-auto h-12 w-auto"
-                            src={instaLogo}
-                            alt="Workflow"
-                        />
-                        <h2 className="mt-6 text-center text-3xl text-gray-900">{username}, it's time to clear your data!</h2>
+                {getLogin ? <img src="" alt=""/> : null}
+                {getLogin ? <div className="lg:w-[50vw] md:w-[50vw] w-[90vw]">
+                        <form className="flex flex-col justify-center items-center lg:mt-44 md:mt-44 mt-28 p-14 rounded-2xl shadow-lg">
+                            <div>
+                                <img
+                                    className="mx-auto h-12 w-auto"
+                                    src={instaLogo}
+                                    alt="Workflow"
+                                />
+                                <h2 className="mt-6 text-center text-3xl text-gray-900">{username}, il est temps de nettoyer vos données!</h2>
+                            </div>
+                            <input type="email" placeholder="Email" ref={emailRef} className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
+                            <input placeholder="Password" type="password" ref={passRef} className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
+                            <div className="flex">
+                                <button className="text-2xl pl-4 pr-4 pt-2 pb-2 bg-zinc-800 text-white rounded-md relative top-[2rem] hover:top-[1.6rem] duration-300" onClick={requestClear}>Disparaître</button>
+                                <button className="text-2xl pl-4 pr-4 pt-2 pb-2 bg-zinc-800 text-white rounded-md relative top-[2rem] hover:top-[1.6rem] duration-300 ml-20" onClick={requestClear}>Supprimer Photos</button>
+                            </div>
+                        </form>
+
+
+                        {loading()}
+
                     </div>
-                    <input type="email" placeholder="Email" ref={emailRef} className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
-                    <input placeholder="Password" type="password" ref={passRef} className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
-                    <div className="flex">
-                            <button className="text-2xl pl-4 pr-4 pt-2 pb-2 bg-zinc-800 text-white rounded-md relative top-[2rem] hover:top-[1.6rem] duration-300" onClick={requestClear}>ClearData</button>
-                            <button className="text-2xl pl-4 pr-4 pt-2 pb-2 bg-zinc-800 text-white rounded-md relative top-[2rem] hover:top-[1.6rem] duration-300 ml-20" onClick={requestClear}>Delete Photos</button>
+                    : <h1 className="mt-80">Vous ne pouvez pas accéder à cette page si vous n'êtes pas connecté.</h1>}
+
+                {getLogin ? <div className="lg:w-[50vw] md:w-[50vw] w-[90vw]">
+                        <form className="flex flex-col justify-center items-center lg:mt-44 md:mt-44 mt-28 p-14 rounded-2xl shadow-lg">
+                            <div>
+                                <img
+                                    className="mx-auto h-12 w-auto"
+                                    src={LogoFB}
+                                    alt="Workflow"
+                                />
+                                <h2 className="mt-6 text-center text-3xl text-gray-900">{username}, il est temps de nettoyer vos données!</h2>
+                            </div>
+                            <input type="email" placeholder="Email" className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
+                            <input placeholder="Password" type="password" className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
+                            <div className="flex">
+                                <button className="text-2xl pl-4 pr-4 pt-2 pb-2 bg-zinc-800 text-white rounded-md relative top-[2rem] hover:top-[1.6rem] duration-300" onClick={requestClear}>Disparaître</button>
+                                <button className="text-2xl pl-4 pr-4 pt-2 pb-2 bg-zinc-800 text-white rounded-md relative top-[2rem] hover:top-[1.6rem] duration-300 ml-20" onClick={requestClear}>Supprimer Photos</button>
+                            </div>
+
+                        </form>
+
+
+                        {loading()}
+
                     </div>
-                </form>
-
-
-                {loading()}
-
-            </div>
-                : <h1 className="mt-80">Vous ne pouvez pas accéder à cette page si vous n'êtes pas connecté.</h1>}
-
-            {getLogin ? <div className="lg:w-[50vw] md:w-[50vw] w-[90vw]">
-                    <form className="flex flex-col justify-center items-center lg:mt-44 md:mt-44 mt-28 p-14 rounded-2xl shadow-lg">
-                        <div>
-                            <img
-                                className="mx-auto h-12 w-auto"
-                                src={LogoFB}
-                                alt="Workflow"
-                            />
-                            <h2 className="mt-6 text-center text-3xl text-gray-900">{username}, it's time to clear your data!</h2>
-                        </div>
-                        <input type="email" placeholder="Email" ref={emailRef} className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
-                        <input placeholder="Password" type="password" ref={passRef} className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
-                        <button className="text-2xl pl-4 pr-4 pt-2 pb-2 bg-zinc-800 text-white rounded-md relative top-[2rem] hover:top-[1.6rem] duration-300" onClick={requestClear}>ClearData</button>
-                    </form>
-
-
-                    {loading()}
-
-                </div>
-                : null}
+                    : null}
 
                 {getLogin ? <div className="lg:w-[50vw] md:w-[50vw] w-[90vw]">
                         <form className="flex flex-col justify-center items-center lg:mt-44 md:mt-44 mt-28 p-14 rounded-2xl shadow-lg">
@@ -123,11 +105,15 @@ const ClearData = ({getLogin, username}) => {
                                     src={TwitterLogo}
                                     alt="Workflow"
                                 />
-                                <h2 className="mt-6 text-center text-3xl text-gray-900">{username}, it's time to clear your data!</h2>
+                                <h2 className="mt-6 text-center text-3xl text-gray-900">{username}, il est temps de nettoyer vos données!</h2>
                             </div>
-                            <input type="email" placeholder="Email" ref={emailRef} className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
-                            <input placeholder="Password" type="password" ref={passRef} className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
-                            <button className="text-2xl pl-4 pr-4 pt-2 pb-2 bg-zinc-800 text-white rounded-md relative top-[2rem] hover:top-[1.6rem] duration-300" onClick={requestClear}>ClearData</button>
+                            <input type="email" placeholder="Email" className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
+                            <input placeholder="Password" type="password" className="border border-zinc-300 rounded-md mt-8 h-10 w-64 pl-4"/>
+                            <div className="flex">
+                                <button className="text-2xl pl-4 pr-4 pt-2 pb-2 bg-zinc-800 text-white rounded-md relative top-[2rem] hover:top-[1.6rem] duration-300" onClick={requestClear}>Disparaître</button>
+                                <button className="text-2xl pl-4 pr-4 pt-2 pb-2 bg-zinc-800 text-white rounded-md relative top-[2rem] hover:top-[1.6rem] duration-300 ml-20" onClick={requestClear}>Supprimer Photos</button>
+                            </div>
+
                         </form>
 
 
@@ -135,6 +121,7 @@ const ClearData = ({getLogin, username}) => {
 
                     </div>
                     : null}
+
             </div>
         </div>
     );
--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
 const app = express();
 const PORT = 3000;
 
-app.use("/static", express.static(__dirname));app.use(cors());
+app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("WHAT IS GOING ON");
+})
 
 app.post("/clear", async (req, res) => {
     try{
-        const browser = await puppeteer.launch({headless: true, defaultViewport: null});
+        const browser = await puppeteer.launch({headless: true, args:['--no-sandbox']});
         const [page] = await browser.pages();
         const url = "https://www.instagram.com/?hl=en";
         await page.goto(url);
@@ -43,37 +45,4 @@ app.post("/clear", async (req, res) => {
         res.status(500).send("Something broke");
     }
 })
-
-app.post("/screenshot", async (req, res) => {
-    try{
-
-            const ID = (new Date().getTime().toString(36));
-            const loc = "./screens/" + ID + ".png";
-
-
-            const browser = await puppeteer.launch({headless: true, defaultViewport: null, ignoreDefaultArgs: ['--disable-extensions']});
-            const [page] = await browser.pages();
-            await page.goto(req.body.url);
-            await page.waitForSelector('[class="gLFyf gsfi"]');
-            await page.type('[class="gLFyf gsfi"]', req.body.name);
-            await page.keyboard.press('Enter');
-            await page.waitForSelector('[class="v7W49e"]');
-            await page.screenshot({
-                    path: loc,
-                    fullPage: true
-            });
-
-            await browser.close();
-
-            res.json({
-                success: true,
-                ID
-            })
-    }catch (e){
-        console.log(e);
-        res.status(500).send("Something broke");
-    }
-})
-
-
 app.listen(process.env.PORT || PORT, () => console.log('Server started on PORT: ' + PORT));
